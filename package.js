bcee1725b050d60f9a75fddfe2beffa00680c03b
konstantin <peredaj@bk.ru>
2016-09-07 11:48:12 +0700

    fix git

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name: 'ksrv:select2-ru',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Language package for jQuery plugin Select2',
-  git: 'git@github.com:ksrv/select2-ru.git',
+  git: 'https://github.com/ksrv/select2-ru.git',
   documentation: 'README.md'
 });
 
